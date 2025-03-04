4033edf3601f0799279f2c8caa747c9c5f18379d
Abdullah Altun <a.altun70@outlook.com>
2025-03-04 16:39:34 +0100

    Added scss animations and made project an npm package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+    implementation: 'sass-embedded',
+  },
+  typescript: {
+    // Ignore type check during build, already performed as part of the CI/CD pipeline
+    ignoreBuildErrors: true,
+  },
 }
 
 export default nextConfig
