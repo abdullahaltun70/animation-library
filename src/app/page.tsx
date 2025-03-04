e5509b8bf748f6c4dc0f295ef71ff1813f56f524
Abdullah Altun <a.altun70@outlook.com>
2025-03-04 16:41:53 +0100

    deleted Fade component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import Image from 'next/image'
 import styles from './page.module.css'
-import Fade from '@/animations/fade'
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Home pagina</h1>
-      <Fade in={true} duration={1} delay={0.5}>
-        <h1>Hello World!</h1>
-      </Fade>
+      <h1>Home page</h1>
     </div>
   )
 }
