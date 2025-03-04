bcbcb489d27a407a361227de0480da336a6c0146
Abdullah Altun <a.altun70@outlook.com>
2025-03-04 11:44:53 +0100

    Add Prettier configuration files and update related files

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+}
